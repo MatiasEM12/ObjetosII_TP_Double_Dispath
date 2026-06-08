69a7d7f04457f68c5bcccab79dc9f1791a8b371f
MatiasEM12 <matiasemimellado@gmail.com>
2026-06-08 19:09:53 -0300

    ejercicio 5 test

--- src/test/java/ejercicio_5/BibliotecaTest.java
+++ src/test/java/ejercicio_5/BibliotecaTest.java
@@ -1,40 +1,110 @@
 package ejercicio_5;
 
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class BibliotecaTest {
 
     @Test
     public void calculoDiasLibroNuevo(){
-        var persona = new Persona();
-        var prestamo = new Prestamo(new Persona(), new Libro(), LocalDate.now());
+        var persona = new Persona("12345678","pepe","argento");
+        var libro = new Libro("El Camino de los Reyes", 1300, new Nuevo());
+        var prestamo = new Prestamo( persona, libro, LocalDate.now());
+
         var fechaDevolucion = prestamo.fechaDevolucion();
 
+        assertEquals(LocalDate.now().plusDays(13),fechaDevolucion);
     }
 
     @Test
-    public void calculoDiasLibroUsado(){}
+    public void calculoDiasLibroUsado(){
+        var persona = new Persona("12345678","pepe","argento");
+        var libro = new Libro("El Camino de los Reyes", 1300, new Usado());
+        var prestamo = new Prestamo( persona, libro, LocalDate.now());
+
+        var fechaDevolucion = prestamo.fechaDevolucion();
+
+        assertEquals(LocalDate.now().plusDays(13),fechaDevolucion);
+    }
 
     @Test
-    public void calculoDiasLibroDeteriorado(){}
+    public void calculoDiasLibroDeteriorado(){
+        var persona = new Persona("12345678","pepe","argento");
+        var libro = new Libro("El Camino de los Reyes", 1300, new Deteriorado());
+        var prestamo = new Prestamo( persona, libro, LocalDate.now());
+
+        var fechaDevolucion = prestamo.fechaDevolucion();
+
+        assertEquals(LocalDate.now().plusDays(13),fechaDevolucion);
+    }
 
     @Test
-    public void calculoDiasDiscoNuevo(){}
+    public void calculoDiasDiscoNuevo(){
+        var persona = new Persona("12345678","pepe","argento");
+        var disco = new Disco("Rock or Bust", 2014, new Nuevo());
+        var prestamo = new Prestamo( persona, disco, LocalDate.now());
+
+        var fechaDevolucion = prestamo.fechaDevolucion();
+
+        assertEquals(LocalDate.now().plusDays(5),fechaDevolucion);
+    }
 
     @Test
-    public void calculoDiasDiscoUsado(){}
+    public void calculoDiasDiscoUsado(){
+        var persona = new Persona("12345678","pepe","argento");
+        var disco = new Disco("Black Sabbath", 1970, new Usado());
+        var prestamo = new Prestamo( persona, disco, LocalDate.now());
+
+        var fechaDevolucion = prestamo.fechaDevolucion();
+
+        assertEquals(LocalDate.now().plusDays(3),fechaDevolucion);
+    }
 
     @Test
-    public void calculoDiasDiscoDeteriorado(){}
+    public void calculoDiasDiscoDeteriorado(){
+        var persona = new Persona("12345678","pepe","argento");
+        var disco = new Disco("Cancion animal", 1990, new Deteriorado());
+        var prestamo = new Prestamo( persona, disco, LocalDate.now());
+
+        var fechaDevolucion = prestamo.fechaDevolucion();
+
+        assertEquals(LocalDate.now().plusDays(4),fechaDevolucion);
+    }
 
     @Test
-    public void calculoDiasRevistaNuevo(){}
+    public void calculoDiasRevistaNuevo(){
+        var persona = new Persona("12345678","pepe","argento");
+        var revista = new Revista(LocalDate.of(2000, 3, 1),50, new Nuevo());
+        var prestamo = new Prestamo( persona, revista, LocalDate.now());
+
+        var fechaDevolucion = prestamo.fechaDevolucion();
+
+        assertEquals(LocalDate.now().plusDays(2),fechaDevolucion);
+    }
 
     @Test
-    public void calculoDiasRevistaUsado(){}
+    public void calculoDiasRevistaUsado(){
+        var persona = new Persona("12345678","pepe","argento");
+        var revista = new Revista(LocalDate.of(2000, 3, 1),350, new Usado());
+        var prestamo = new Prestamo( persona, revista, LocalDate.now());
+
+        var fechaDevolucion = prestamo.fechaDevolucion();
+
+        assertEquals(LocalDate.now().plusDays(3),fechaDevolucion);
+    }
 
     @Test
-    public void calculoDiasRevistaDeteriorado(){}
+    public void calculoDiasRevistaDeteriorado(){
+        var persona = new Persona("12345678","pepe","argento");
+        var revista = new Revista(LocalDate.of(2000, 3, 1),350, new Deteriorado());
+        var prestamo = new Prestamo( persona, revista, LocalDate.now());
+
+        var fechaDevolucion = prestamo.fechaDevolucion();
+
+        assertEquals(LocalDate.now().plusDays(1),fechaDevolucion);
+    }
 }
